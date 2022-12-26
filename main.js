548dc7e606d65beab5afb0d17cd9230564330a44
DavidUmansky <71831586+DavidUmansky@users.noreply.github.com>
2022-12-27 01:50:53 +0200

    update

--- main.js
+++ main.js
@@ -7,7 +7,53 @@ const nav = document.getElementById('header');
 section.style.height = div.offsetWidth + 'px';
 
 window.addEventListener('scroll', function () {
-    let offset = window.scrollY;
-    parallax.style.backgroundPositionY = offset * -0.1 + 'px';
+    // let offset = window.scrollY;
+    // parallax.style.backgroundPositionY = offset * -0.1 + 'px';
+    // console.log(window.pageYOffset);
+    if (window.pageYOffset > 900) {
+        let offset = window.scrollY;
+        parallax.style.backgroundPositionY = offset * -0.15 + 100 + 'px';
+    } 
 })
+
+const swiper = new Swiper('.swiper', {
+
+    // autoplay: {
+    //     delay: 3000,
+    //     disableOnInteraction: false,
+    // },
+
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
